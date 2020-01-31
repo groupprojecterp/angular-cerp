ff5527e0ab41a2008b2241a8cd1b51ee1fc5dc15
groupprojecterp <groupprojecterp@gmail.com>
2020-01-31 14:38:37 +0530

    3 files modified

--- src/app/library/book-issue/book-issue.component.ts
+++ src/app/library/book-issue/book-issue.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import {BooksearchService} from '../booksearch/booksearch.service';
+import { DatePipe } from '@angular/common';
+
 @Component({
   selector: 'app-book-issue',
   templateUrl: './book-issue.component.html',
-  styleUrls: ['./book-issue.component.css']
+  styleUrls: ['./book-issue.component.css'],
+  providers: [DatePipe]
 })
 export class BookIssueComponent implements OnInit {
   author: string
   pages: number
   title: string
   year: number
-  constructor(private _temp:BooksearchService) { 
+  studentId:string
+  date:Date
+  
+  constructor(private _temp:BooksearchService ,private date_pipe:DatePipe) { 
     this._temp.getBooks().subscribe(data=>{
       for(let i=0;i<data.length;i++){
         if(data[i].key==this._temp.issueKey){
@@ -26,6 +32,8 @@ export class BookIssueComponent implements OnInit {
     this.author = 'jk rowling';
     this.pages = 300;
     this.year = 2019;
+    this.date = new Date();
+    console.log(date_pipe.transform(this.date,'dd-MM-yyyy'))
 
   }
   ngDoCheck(){
