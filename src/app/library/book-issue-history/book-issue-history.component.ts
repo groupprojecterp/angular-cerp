e7f08e359be81b20792223ce018b40e97bac8974
groupprojecterp <groupprojecterp@gmail.com>
2020-02-03 00:08:48 +0530

    2 files modified

--- src/app/library/book-issue-history/book-issue-history.component.ts
+++ src/app/library/book-issue-history/book-issue-history.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {BooksearchService} from '../booksearch/booksearch.service';
-import {HttpClient} from '@angular/common/http';
+import {FirebaseListObservable} from '@angular/fire/database';
+
 @Component({
   selector: 'app-book-issue-history',
   templateUrl: './book-issue-history.component.html',
@@ -8,10 +9,10 @@ import {HttpClient} from '@angular/common/http';
 })
 export class BookIssueHistoryComponent implements OnInit {
   issueHistoryList:IssueHistoryObj[]
-
-  constructor(private search:BooksearchService,private http:HttpClient) {
+  constructor(private search:BooksearchService) {
     
-    search.getIssues().subscribe(data=>{
+
+    search.getIssues().forEach(data=>{
       this.issueHistoryList = new Array()
       for(let i=0;i<data.length;i++){
         let val = data[i].payload.exportVal()
@@ -20,55 +21,53 @@ export class BookIssueHistoryComponent implements OnInit {
           book_id:val.book_id,
           return_date:val.return_date,
           book_title:'',
-          student_name:'',
-          email:''
+          student_name:''
         })
 
       }
       
-    
+      search.getStudents().forEach(data=>{
+      for(let i=0;i<data.length;i++){
+        var val = data[i].payload.exportVal()
+        
+        for(let j=0;j<this.issueHistoryList.length;j++){
       
+          if(this.issueHistoryList[j].student_id==val.student_id){
+            this.issueHistoryList[j].student_name=val.name
+          }
+        }
+
+      }
+
     }) 
-    this.search.getBooks().subscribe(data=>{
-        for(let i=0;i<data.length;i++){
-          let val = data[i].payload.exportVal()
-          for(let j=0;j<this.issueHistoryList.length;i++){
-            if(val.book_id==this.issueHistoryList[j].book_id){
-              this.issueHistoryList[j].book_title = val.title
-            }
+    search.getBooks().forEach(data=>{
+      for(let i=0;i<data.length;i++){
+        var val = data[i].payload.exportVal()
+        
+        for(let j=0;j<this.issueHistoryList.length;j++){
+      
+          if(this.issueHistoryList[j].book_id==val.book_id){
+            this.issueHistoryList[j].book_title=val.title
           }
         }
-      })
-      this.search.getStudents().subscribe(data=>{
-        for(let i=0;i<data.length;i++){
-          let val = data[i].payload.exportVal()
-          for(let j=0;j<this.issueHistoryList.length;i++){
-            if(val.student_id==this.issueHistoryList[j].student_id){
-              this.issueHistoryList[j].student_id = val.name
-              this.issueHistoryList[j].email = val.email
-            }
-          }
-        }
-      })
-    
+
+      }
+
+    })
+    }) 
+     
 
 
    }
 
   ngOnInit() {
   }
-  sendMail(row){
-    let url = 'https://us-central1-college-erp-668a1.cloudfunctions.net/sendMail?'
-    url = url+'dest='+row.email+'&title='+row.book_title+'&name='+row.student_name+'&date='+row.return_date
-     this.http.get(url).subscribe()
-  }
 
 }
-interface IssueHistoryObj{
+class IssueHistoryObj{
   student_name:string
   student_id:string
   book_title:string
   book_id:string
   return_date:string
-  email:string
 }
